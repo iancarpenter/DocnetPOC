8cbee0d7952f55488ab48a3d2c13ca4b6d7c4a5e
iancarpenter <ianhcarpenter@gmail.com>
2023-01-09 21:31:38 +0000

    Initial version

---
+++ DocnetPOC/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Docnet.Core;
+using Docnet.Core.Models;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var docReader = DocLib.Instance.GetDocReader(@"C:\Temp\EMegane.pdf", new PageDimensions()))
+            {
+                for (var i = 0; i < docReader.GetPageCount(); i++)
+                {
+                    using (var pageReader = docReader.GetPageReader(i))
+                    {
+                        var text = pageReader.GetText();
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+
+        }
+    }
+}
